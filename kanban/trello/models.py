52e569619518329281992a4709985e3e4ea63cf0
Nathan Rogers <nrogers@nrogers-macbookpro.roam.corp.google.com>
2016-08-23 15:37:11 -0400

    Added basic models for lists and cards.

--- kanban/trello/models.py
+++ kanban/trello/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # Create your models here.
+class TaskList(models.Model):
+    name = models.CharField(max_length=200, editable=True)
+
+    def __str__(self):
+        return "Name: {} | Cards: {}".format(self.name, self.card_set.all())
+
+
+class Card(models.Model):
+    task_list = models.ForeignKey('TaskList', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, editable=True)
+    description = models.TextField(default='')
+    timestamp = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField(blank=True, null=True)
+
+    activity_log = models.TextField(default="Card created | {}".format(
+                                                            timezone.now()))
+
+    def __str__(self):
+        if self.due_date is None:
+            return "Name: {} | Description: {}".format(self.name,
+                                                       self.description)
+        else:
+            return "Name: {} | Description: {} | Due Date: {}".format(
+                                                            self.name,
+                                                            self.description,
+                                                            self.due_date)
