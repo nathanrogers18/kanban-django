ff760103dfa057754e54c48a22a55f8ed0719656
Nathan Rogers <nrogers@nrogers-macbookpro.roam.corp.google.com>
2016-08-23 15:57:19 -0400

    Added views

--- kanban/trello/serializers.py
+++ kanban/trello/serializers.py
@@ -5,10 +5,11 @@ from rest_framework import serializers
 class TaskListSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = TaskList
-        fields = ('first_name', 'last_name', 'age', 'favorite_color')
+        fields = ('name',)
 
 
 class CardSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Card
-        fields = ('first_name', 'last_name', 'age', 'favorite_color')
+        fields = ('task_list', 'name', 'description', 'timestamp',
+                  'due_date', 'activity_log')
--- kanban/trello/views.py
+++ kanban/trello/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import TaskList, Card
+from rest_framework import viewsets
+from .serializers import PersonSerializer
 
 # Create your views here.
+def index(request):
+    return render(request, 'editable/index.html')
+
+
+class TaskListViewSet(viewsets.ModelViewSet):
+    queryset = TaskList.objects.all()
+    serializer_class = TaskListSerializer
+
+
+class CardViewSet(viewsets.ModelViewSet):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
