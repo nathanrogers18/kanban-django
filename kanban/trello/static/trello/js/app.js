36dc1b4a099617ad98e46a4d21e2ead668af2c8b
Nathan <nathanrogers18@gmail.com>
2016-08-24 13:49:22 -0400

    Updated serializers and added JS to update Card List Titles

--- kanban/trello/static/trello/js/app.js
+++ kanban/trello/static/trello/js/app.js
@@ -8,6 +8,35 @@ $.ajax({
             var $col = $('<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">');
             var $card = $('<div class="card card-block">').appendTo($col);
             var $cardTitle = $('<h4 class="card-title">').text(result.name).appendTo($card);
+            $cardTitle.click(function(){
+                var listName = $cardTitle.text();
+                $cardTitle.empty();
+                var $form = $('<form>').appendTo($cardTitle);
+
+                var $input = $('<input type="text">').val(listName);
+
+                $input.click(function(){
+                    return false;
+                });
+
+                $input.appendTo($form);
+
+                $form.submit(function(){
+                    var listName = $input.val();
+                    console.log(result)
+                    $.ajax({
+                        method: 'PUT',
+                        url: 'api/tasks/' + result.id + '/',
+                        data : {
+                            name: listName
+                        }
+                    });
+                    $cardTitle.empty();
+                    $cardTitle.text(listName);
+                    return false;
+                });
+            });
+
             var $ul = $('<ul class="list-group list-group-flush">').appendTo($card);
             console.log('yay');
             console.log(result.card_set);
@@ -21,12 +50,8 @@ $.ajax({
             var $addCard = $('<a href="#" class="card-link">Add a card</a>').appendTo($card);
             var $delList = $('<a href="#" class="card-link">Delete list</a>').appendTo($card);
             $col.appendTo($task_list);
-
-
         });
-
     }
-
 });
 
 // <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
